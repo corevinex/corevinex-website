79777a134d27ac438841870a50eca0772fa57ed7
corevinex <vikasckonline@gmail.com>
2026-06-30 09:27:40 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -114,3 +114,18 @@ document.querySelectorAll('.reveal').forEach(el =>
 
   counters.forEach(c => io.observe(c));
 })();
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mousemove', e => {
+        const rect = card.getBoundingClientRect();
+
+        card.style.setProperty(
+            '--x',
+            `${e.clientX - rect.left}px`
+        );
+
+        card.style.setProperty(
+            '--y',
+            `${e.clientY - rect.top}px`
+        );
+    });
+});
